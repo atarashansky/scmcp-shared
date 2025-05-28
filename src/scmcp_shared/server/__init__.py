d84add6f574feb7984bf62de806d20b82429131f
shuang <shenghui.huang@unito.it>
2025-05-28 23:44:33 +0000

    update

--- src/scmcp_shared/server/__init__.py
+++ src/scmcp_shared/server/__init__.py
@@ -1,51 +1,13 @@
 from __future__ import annotations
 
-from collections.abc import Iterable
-from typing import Any
+from collections.abc import Iterable, AsyncIterator
+from typing import Any, Dict, List, Optional
+from contextlib import asynccontextmanager
+import asyncio
 
-from .io import io_mcp
-from .util import ul_mcp, ScanpyUtilMCP
-from .pl import pl_mcp, ScanpyPlottingMCP
-from .pp import pp_mcp, ScanpyPreprocessingMCP
-from .tl import tl_mcp, ScanpyToolsMCP
-
-
-
-class AdataState:
-    def __init__(self, add_adtypes=None):
-        self.adata_dic = {"exp": {}, "activity": {}, "cnv": {}, "splicing": {}}
-        if isinstance(add_adtypes, str):
-            self.adata_dic[add_adtypes] = {}
-        elif isinstance(add_adtypes, Iterable):
-            self.adata_dic.update({adtype: {} for adtype in add_adtypes})
-        self.active_id = None
-        self.metadatWa = {}
-        self.cr_kernel = {}
-        self.cr_estimator = {}
-        
-    def get_adata(self, sampleid=None, adtype="exp", adinfo=None):
-        if adinfo is not None:
-            kwargs = adinfo.model_dump()
-            sampleid = kwargs.get("sampleid", None)
-            adtype = kwargs.get("adtype", "exp")
-        try:
-            if self.active_id is None:
-                return None
-            sampleid = sampleid or self.active_id
-            return self.adata_dic[adtype][sampleid]
-        except KeyError as e:
-            raise KeyError(f"Key {e} not found in adata_dic[{adtype}].Please check the sampleid or adtype.")
-        except Exception as e:
-            raise Exception(f"fuck {e} {type(e)}")
-    
-    def set_adata(self, adata, sampleid=None, sdtype="exp", adinfo=None):
-        if adinfo is not None:
-            kwargs = adinfo.model_dump()
-            sampleid = kwargs.get("sampleid", None)
-            sdtype = kwargs.get("adtype", "exp")
-        sampleid = sampleid or self.active_id
-        if sdtype not in self.adata_dic:
-            self.adata_dic[sdtype] = {}
-        self.adata_dic[sdtype][sampleid] = adata
-                          
-
+from .base import BaseMCP,AdataState,BaseMCPManager
+from .io import ScanpyIOMCP, io_mcp
+from .util import ScanpyUtilMCP
+from .pl import ScanpyPlottingMCP
+from .pp import ScanpyPreprocessingMCP
+from .tl import ScanpyToolsMCP
