0437fd925af3838798ecbf9995a51904c2417893
shuang <shenghui.huang@unito.it>
2025-06-12 10:24:09 +0000

    move openai client to function

--- src/scmcp_shared/agent.py
+++ src/scmcp_shared/agent.py
@@ -4,16 +4,15 @@ from scmcp_shared.schema.tool import ToolList
 import os
 
 
-API_KEY = os.environ.get("API_KEY",  None)
-BASE_URL = os.environ.get("BASE_URL", None)
-MODEL = os.environ.get("MODEL", None)
-
-
-client = OpenAI(api_key=API_KEY, base_url=BASE_URL)
-client = instructor.from_openai(client)
-
 
 def select_tool(query):
+
+    API_KEY = os.environ.get("API_KEY",  None)
+    BASE_URL = os.environ.get("BASE_URL", None)
+    MODEL = os.environ.get("MODEL", None)
+
+    client = OpenAI(api_key=API_KEY, base_url=BASE_URL)
+    client = instructor.from_openai(client)
 
     response = client.chat.completions.create(
         model=MODEL,
