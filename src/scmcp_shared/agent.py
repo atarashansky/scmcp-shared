5e85378ffd61838fd150a229e371a4eac690a041
shuang <shenghui.huang@unito.it>
2025-07-07 16:25:21 +0000

    update rag_agent prompt

--- src/scmcp_shared/agent.py
+++ src/scmcp_shared/agent.py
@@ -27,31 +27,9 @@ def rag_agent(task, software=None):
     <task>
     {task}
     </task>
-    拆分任务，分别查询知识库，查找完成每个子任务所需要的函数以及参数, 给出一个总的用于解决任务的代码示例。返回结果格式为：
-    <function_list>
-        <function>
-            <package>.<module>.<function_name>
-            <package>.<module>.<function_name>(<param1>[, <param2>=<default>][, ...][, **kwargs])
-            <一句话描述函数的主要功能>。
-
-            <可选：补充说明/实现细节/依赖等>
-
-            Parameters
-            :
-            <param1> (<type>) – <param1 的说明>
-
-            <param2> (<type> (default: <default>)) – <param2 的说明>
-
-            <param3> (<type> (default: <default>)) – <param3 的说明>
-
-            ...（按需继续列出其他参数）
-
-            **kwargs – <对底层实现传递的其他关键字参数的说明；如无需说明可简写为“其他关键字参数”。>
-        </function>
-                        ...
-    </function_list>
+    查询知识库，给出一个用于解决任务的代码示例。返回结果格式为：
     <code_example>
-    [code_example]
+        [code_example]
     </code_example>
     """
     rep = agent.run(query)
